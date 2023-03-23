4452d3b064c4088f3771d4eab13d6b39791c4502
Yannick Peeters <y.peeters@edna.eu>
2023-03-23 11:53:04 +0100

    Trait bound syntax.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
-use std::io;
+trait Printer {
+    fn print(&self);
+}
+
+struct A {}
+impl Printer for A {
+    fn print(&self) {
+        println!("A");
+    }
+}
+struct B {}
+impl Printer for B {
+    fn print(&self) {
+        println!("B");
+    }
+}
+
+fn do_print0(p: &impl Printer) {
+    p.print();
+}
+
+fn do_print1<T: Printer>(p: &T) {
+    p.print();
+}
+
+fn do_print2<T>(p: &T)
+where
+    T: Printer,
+{
+    p.print();
+}
 
 fn main() {
-    println!("{:?}", return_error_or_ok(true));
-    println!("{:?}", return_error_or_ok(false));
-    println!("{:?}", return_some_or_none(true));
-    println!("{:?}", return_some_or_none(false));
-}
-
-fn return_some_or_none(some: bool) -> Option<u32> {
-    return_error(some).ok()
-}
-fn return_error_or_ok(ok: bool) -> Result<u32, ()> {
-    return_error(ok).map_err(|_| ())
-}
-
-fn return_error(error: bool) -> Result<u32, io::Error> {
-    if error {
-        Ok(5)
-    } else {
-        Err(io::Error::new(io::ErrorKind::AddrInUse, "io error"))
-    }
+    let a = A {};
+    let b = B {};
+    do_print0(&a);
+    do_print0(&b);
+    do_print1(&a);
+    do_print1(&b);
+    do_print2(&a);
+    do_print2(&b);
 }
