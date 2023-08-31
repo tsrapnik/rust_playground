811fecfe92b7053964f17f59609acece9550c1b9
Yannick Peeters <y.peeters@edna.eu>
2023-08-31 11:23:28 +0200

    anyhow error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use anyhow::Result;
+use std::{error::Error, fmt, fmt::Display};
+
 use iced::{
-    widget::{button, column, radio, text, text_input, text_input::StyleSheet},
-    Element, Sandbox, Settings,
+    widget::{button, column, radio, text, text_input},
+    Element, Sandbox,
 };
+
 struct Counter {
     value: i32,
     s: String,
@@ -29,8 +33,9 @@ impl Sandbox for Counter {
     }
 
     fn view(&self) -> Element<Message> {
+        let text_input = text_input("Type something.", &self.s);
         column!(
-            text_input("Type something.", &self.s).on_input(|x| Message::InputReceived(x)),
+            text_input.on_input(|x| Message::InputReceived(x)),
             button("+").on_press(Message::IncrementPressed),
             text(self.value).size(50),
             button("-").on_press(Message::DecrementPressed),
@@ -57,5 +62,62 @@ impl Sandbox for Counter {
 }
 
 fn main() {
-    Counter::run(Settings::default()).unwrap();
+    // Counter::run(Settings::default()).unwrap();
+    println!("{:?}", error_wrapper(WhichError::First));
+    println!("{:?}", error_wrapper(WhichError::Second));
+    println!("{:?}", error_wrapper(WhichError::None));
+}
+
+#[derive(Debug)]
+struct MyError1 {
+    number: u32,
+}
+
+impl Display for MyError1 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "error number {}", self.number)
+    }
+}
+
+impl Error for MyError1 {}
+
+#[derive(Debug)]
+struct MyError2 {
+    number: u32,
+}
+
+impl Display for MyError2 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "error number {}", self.number)
+    }
+}
+
+impl Error for MyError2 {}
+
+fn error_or_not_1(throw_error: bool) -> Result<u32, MyError1> {
+    if throw_error {
+        Err(MyError1 { number: 4 })
+    } else {
+        Ok(8)
+    }
+}
+
+fn error_or_not_2(throw_error: bool) -> Result<u32, MyError2> {
+    if throw_error {
+        Err(MyError2 { number: 6 })
+    } else {
+        Ok(10)
+    }
+}
+
+enum WhichError {
+    First,
+    Second,
+    None,
+}
+
+fn error_wrapper(which_error: WhichError) -> Result<u32> {
+    let x = error_or_not_1(matches!(which_error, WhichError::First))?;
+    let y = error_or_not_2(matches!(which_error, WhichError::Second))?;
+    Ok(u32::max(x, y))
 }
