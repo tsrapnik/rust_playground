46d4a6cab16119fd1a751de3fab3ac5673556c2c
Yannick Peeters <y.peeters@edna.eu>
2023-09-29 11:45:44 +0200

    Add custom theme.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use self::theme::Theme;
+use self::widget::Element;
 use iced::{
-    theme,
-    widget::{button, column, radio, text, text_input},
-    Color, Element, Sandbox, Settings,
+    executor,
+    widget::{button, column, text, text_input},
+    Application, Command, Settings,
 };
 
 struct Counter {
@@ -16,30 +18,27 @@ pub enum Message {
     InputReceived(String),
 }
 
-impl Sandbox for Counter {
+impl Application for Counter {
+    type Executor = executor::Default;
     type Message = Message;
+    type Theme = Theme;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self {
-            value: 0,
-            s: "".into(),
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        (
+            Self {
+                value: 0,
+                s: "".into(),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         String::from("test application")
     }
 
-    fn theme(&self) -> iced::Theme {
-        let mut my_palette = theme::Theme::Dark.palette();
-        my_palette.background = Color::from_rgb(1.0, 1.0, 1.0);
-        my_palette.primary = Color::from_rgb(1.0, 0.0, 1.0);
-        my_palette.danger = Color::from_rgb(1.0, 0.0, 0.0);
-        my_palette.success = Color::from_rgb(0.0, 1.0, 0.0);
-        my_palette.text = Color::from_rgb(0.2, 0.2, 0.2);
-
-        theme::Theme::custom(my_palette)
-    }
+    // fn theme(&self) -> iced::Theme {}
 
     fn view(&self) -> Element<Message> {
         let text_input = text_input("Type something.", &self.s);
@@ -49,30 +48,19 @@ impl Sandbox for Counter {
         let min_button_1 = button("-")
             .on_press(Message::DecrementPressed)
             .style(theme::Button::Secondary);
-        let min_button_2 = button("-")
-            .on_press(Message::DecrementPressed)
-            .style(theme::Button::Positive);
-        let min_button_3 = button("-")
-            .on_press(Message::DecrementPressed)
-            .style(theme::Button::Destructive);
         column!(
             text_input.on_input(|x| Message::InputReceived(x)),
             button("+")
                 .on_press(Message::IncrementPressed)
-                .style(theme::Button::Text),
+                .style(theme::Button::Primary),
             text(self.value).size(50),
             min_button_0,
             min_button_1,
-            min_button_2,
-            min_button_3,
-            radio(String::from("label"), true, None, |_x| {
-                Message::DecrementPressed
-            }),
         )
         .into()
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::IncrementPressed => {
                 self.value += 1;
@@ -84,9 +72,142 @@ impl Sandbox for Counter {
                 self.s = s;
             }
         }
+        Command::none()
     }
 }
 
 fn main() {
     Counter::run(Settings::default()).unwrap();
+}
+
+mod widget {
+    #![allow(dead_code)]
+    use crate::{theme::Theme, Message};
+
+    pub type Renderer = iced::Renderer<Theme>;
+    pub type Element<'a, Message> = iced::Element<'a, Message, Renderer>;
+    pub type Button<'a, Message> = iced::widget::Button<'a, Message, Renderer>;
+    pub type TextInput<'a, Message> = iced::widget::TextInput<'a, Message, Renderer>;
+    pub type Text<'a> = iced::widget::Text<'a, Renderer>;
+    pub type Column<'a> = iced::widget::Column<'a, Message, Renderer>;
+}
+
+mod theme {
+    use iced::{
+        application,
+        widget::{button, text, text_input},
+        Color,
+    };
+
+    #[derive(Debug, Clone, Copy, Default)]
+    pub struct Theme;
+
+    impl application::StyleSheet for Theme {
+        type Style = ();
+
+        fn appearance(&self, _style: &Self::Style) -> application::Appearance {
+            application::Appearance {
+                background_color: Color::from_rgb(0.3, 0.3, 0.3),
+                text_color: Color::from_rgb(0.8, 0.8, 0.8),
+            }
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, Default)]
+    pub enum Button {
+        #[default]
+        Primary,
+        Secondary,
+    }
+
+    impl button::StyleSheet for Theme {
+        type Style = Button;
+
+        fn active(&self, style: &Self::Style) -> button::Appearance {
+            match style {
+                Button::Primary => button::Appearance {
+                    background: Some(Color::from_rgb(0.8, 0.3, 0.3).into()),
+                    ..Default::default()
+                },
+                Button::Secondary => button::Appearance {
+                    background: Some(Color::from_rgb(0.3, 0.8, 0.3).into()),
+                    ..Default::default()
+                },
+            }
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, Default)]
+    pub enum TextInput {
+        #[default]
+        Default,
+    }
+
+    impl text_input::StyleSheet for Theme {
+        type Style = TextInput;
+
+        fn active(&self, style: &Self::Style) -> text_input::Appearance {
+            text_input::Appearance {
+                background: Color::from_rgb(0.3, 0.8, 0.3).into(),
+                border_radius: 1.0.into(),
+                border_width: 1.0,
+                border_color: Color::from_rgb(0.3, 0.8, 0.3).into(),
+                icon_color: Color::from_rgb(0.3, 0.8, 0.3).into(),
+            }
+        }
+
+        fn focused(&self, style: &Self::Style) -> text_input::Appearance {
+            text_input::Appearance {
+                background: Color::from_rgb(0.3, 0.8, 0.3).into(),
+                border_radius: 1.0.into(),
+                border_width: 1.0,
+                border_color: Color::from_rgb(0.3, 0.8, 0.3).into(),
+                icon_color: Color::from_rgb(0.3, 0.8, 0.3).into(),
+            }
+        }
+
+        fn placeholder_color(&self, style: &Self::Style) -> Color {
+            Color::from_rgb(1.0, 0.0, 0.0).into()
+        }
+
+        fn value_color(&self, style: &Self::Style) -> Color {
+            Color::from_rgb(0.0, 0.0, 0.0).into()
+        }
+
+        fn disabled_color(&self, style: &Self::Style) -> Color {
+            Default::default()
+        }
+
+        fn selection_color(&self, style: &Self::Style) -> Color {
+            Default::default()
+        }
+
+        fn disabled(&self, style: &Self::Style) -> text_input::Appearance {
+            text_input::Appearance {
+                background: Color::from_rgb(0.3, 0.8, 0.3).into(),
+                border_radius: 1.0.into(),
+                border_width: 1.0,
+                border_color: Color::from_rgb(0.3, 0.8, 0.3).into(),
+                icon_color: Color::from_rgb(0.3, 0.8, 0.3).into(),
+            }
+        }
+    }
+
+    #[derive(Debug, Clone, Copy, Default)]
+    pub enum Text {
+        #[default]
+        Default,
+    }
+
+    impl text::StyleSheet for Theme {
+        type Style = Text;
+
+        fn appearance(&self, style: Self::Style) -> text::Appearance {
+            match style {
+                Text::Default => text::Appearance {
+                    color: Color::from_rgb(0.8, 0.8, 0.8).into(),
+                },
+            }
+        }
+    }
 }
